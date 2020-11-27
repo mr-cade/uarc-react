21b8a22b973d98b743ccb76f6f8b6d23a6d023a9
mr-cade <cadelpetersen@gmail.com>
2020-11-27 09:57:51 -0700

    blank pages and routes on app.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+import home from "./pages/home"
+import about from "./pages/about"
+import atv from "./pages/atv"
+import benefits from "./pages/benefits"
+import books from "./pages/books"
+import elmers from "./pages/elmers"
+import irlp from "./pages/irlp"
+import join from "./pages/join"
+import lending from "./pages/lending"
+import meeting from "./pages/meeting"
+import repeaters from "./pages/repeaters"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+      <Navbar/>
+        <Route exact path="/" component={home} />
+        <Route exact path="/about" component={about} />
+        <Route exact path="/atv" component={atv} />
+        <Route exact path="/benefits" component={benefits} />  
+        <Route exact path="/books" component={books} />
+        <Route exact path="/elmers" component={elmers} />
+        <Route exact path="/irlp" component={irlp} />
+        <Route exact path="/join" component={join} />
+        <Route exact path="/lending" component={lending} />
+        <Route exact path="/meeting" component={meeting} />
+        <Route exact path="/repeaters" component={repeaters} />
+
+      <Footer/>
+      </Router>
     </div>
   );
 }
