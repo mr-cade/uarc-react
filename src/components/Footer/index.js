6ec4529b6380f1d8393157af440729a998db9a04
mr-cade <cadelpetersen@gmail.com>
2020-11-30 19:07:29 -0700

    footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,10 +1,26 @@
 import React from "react";
 import "./style.css";
+import { FaFacebookSquare, FaYoutube } from 'react-icons/fa';
 
 export default function Footer() {
     return (
-     <div>
-         
-     </div>   
+        <footer className="socials page-footer">
+            <a
+            className="mr-3"
+            style={{color: 'white'}}
+            href="https://www.facebook.com/UtahAmateurRadioClub/"
+            target="_blank"
+            rel="noopener noreferrer">
+                <FaFacebookSquare size={40}/>
+            </a>
+            <a
+            className=""
+            style={{color: 'white'}}
+            href="https://www.youtube.com/c/utahamateurradioclub"
+            target="_blank"
+            rel="noopener noreferrer">
+                <FaYoutube size={40}/>
+            </a>
+        </footer>
     )
 }
