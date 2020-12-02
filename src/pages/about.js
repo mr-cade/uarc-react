c965b4efb9050e8da8b8829b5e7c953ab4839c80
mr-cade <cadelpetersen@gmail.com>
2020-12-02 14:25:46 -0700

    about history page static content

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,6 +1,22 @@
 import React from 'react'
 
 export default function About() {
-    return(
-        <div>About Page Content</div>
-        )}
+    return (
+        <div>
+            <h1>Club Information</h1>
+                <p>
+                    The Utah Amateur Radio Club (UARC) is a general-purpose amateur radio club serving Salt Lake City and nearby areas. It is a Utah non-profit corporation. Meetings are held on the second Thursday of each month, except for July and August. Meeting topics include many amateur-related items such as DX, satellites, Amateur Television, antennas, and contests. A copy of the club <a href="link">By-Laws is available here.</a> 
+                </p>
+            <h1>Club History</h1>
+                <p>
+                    In his book 200 Meters and Down, Clinton Desoto describes a radio club in Salt Lake City as the very first organization of its kind. That organization was formed in 1909. It is possible that UARC's roots go back to that organization, but the records extant show that the Utah Amateur Radio Club was formed under its current name in 1927. The club still has records containing the minutes of the first meeting. UARC became affiliated with the American Radio Relay League the following year. <a href="link">(You can join ARRL through the club.)</a> In 1957 the club incorporated and began publishing its current newsletter, The Microvolt.
+                </p>
+                <p>
+                    UARC holds the club callsign <b>W7SP</b>, which is a memorial call for Leonard (Zim) Zimmerman. Zim was active in amateur radio back in the days when one had to grind his own crystals from the raw quartz rock. Virtually every piece of amateur radio equipment Zim used was home-built (He used the label "ZimMade"). The equipment generally sounded better than the commercial rigs of the day. UARC is proud to honor Zim by using the W7SP call on its repeaters, its Field Day stations, and various special event stations.
+                </p>
+                <p>
+                    In 2002, on the occasion of UARC's seventy-fifth anniversary, Alan Seyboldt, N7OI, went through the club archives and wrote a synopsis of the club's history, available <a href="link">here</a>. It gives a more detailed history of UARC and shows how much the club and the amateur radio hobby have changed over the years.
+                </p>
+        </div>
+    )
+}
