87e96e6695511bc8d6cc3a38940bf26f2260761a
mr-cade <cadelpetersen@gmail.com>
2020-12-03 14:58:23 -0700

    elmer table

--- client/src/components/Elmer-table/index.js
+++ client/src/components/Elmer-table/index.js
@@ -3,8 +3,401 @@ import "./style.css";
 
 export default function ElmerTable() {
     return (
-     <div>
-         Elmers Content
-     </div>   
+        <div>
+            <table class="table">
+                <tr>
+                    <th>
+                        Topic
+                    </th>
+
+                    <th>
+                        Elmer Name
+                    </th>
+
+                    <th>
+                        Email
+                    </th>
+
+                    <th>
+                        Phone
+                    </th>
+                </tr>
+                <tr>
+                    <td>ATV (Fast Scan)</td>
+
+                    <td>Clint Turner – KA7OEI</td>
+
+                    <td>See<a href="http://www.ussc.com/~turner/email.html">
+                        www.ussc.com/~turner/email.html</a></td>
+
+                    <td>&nbsp;</td>
+                </tr>
+
+                <tr>
+                    <td>Antennas</td>
+
+                    <td>&nbsp;</td>
+
+                    <td>&nbsp;</td>
+
+                    <td>&nbsp;</td>
+                </tr>
+
+                <tr>
+                    <td>APRS</td>
+
+                    <td>Steve Baxter - K7SRB</td>
+
+                    <td>&nbsp;</td>
+
+                    <td>&nbsp;</td>
+                </tr>
+
+                <tr>
+                    <td>Awards</td>
+
+                    <td>&nbsp;</td>
+
+                    <td>&nbsp;</td>
+
+                    <td>&nbsp;</td>
+                </tr>
+
+                <tr>
+                    <td>Circuit Design</td>
+
+                    <td>&nbsp;</td>
+
+                    <td>&nbsp;</td>
+
+                    <td>&nbsp;</td>
+                </tr>
+
+                <tr>
+                    <td>CW</td>
+
+                    <td>Gary Openshaw – KC7AWU</td>
+
+                    <td>&nbsp;</td>
+
+                    <td>&nbsp;</td>
+                </tr>
+
+                <tr>
+                    <td>DFing</td>
+
+                    <td>Jerry Wellman – W7SAR</td>
+
+                    <td>&nbsp;</td>
+
+                    <td>&nbsp;</td>
+                </tr>
+
+                <tr>
+                    <td>DSP</td>
+
+                    <td>&nbsp;</td>
+
+                    <td>&nbsp;</td>
+
+                    <td>&nbsp;</td>
+                </tr>
+
+                <tr>
+                    <td>DXing</td>
+
+                    <td>&nbsp;</td>
+
+                    <td>&nbsp;</td>
+
+                    <td>&nbsp;</td>
+                </tr>
+
+                <tr>
+                    <td>EME</td>
+
+                    <td>&nbsp;</td>
+
+                    <td>&nbsp;</td>
+
+                    <td>&nbsp;</td>
+                </tr>
+
+                <tr>
+                    <td>EMI</td>
+
+                    <td>&nbsp;</td>
+
+                    <td>&nbsp;</td>
+
+                    <td>&nbsp;</td>
+                </tr>
+
+                <tr>
+                    <td>Emergency Communications</td>
+
+                    <td>Jerry Wellman – W7SAR</td>
+
+                    <td>&nbsp;</td>
+
+                    <td>&nbsp;</td>
+                </tr>
+
+                <tr>
+                    <td>HF Contesting</td>
+
+                    <td>&nbsp;</td>
+
+                    <td>&nbsp;</td>
+
+                    <td>&nbsp;</td>
+                </tr>
+
+                <tr>
+                    <td>HF Digital</td>
+
+                    <td>Ron Speirs – K7RLS</td>
+
+                    <td>&nbsp;</td>
+
+                    <td>&nbsp;</td>
+                </tr>
+
+                <tr>
+                    <td>HF Mobile</td>
+
+                    <td>John Hays – K7VE
+                    <br />Brett Sutherland – N7KG n7kg@arrl.net
+                    <br />Jerry Wellman – W7SAR</td>
+
+                    <td>&nbsp;</td>
+
+                    <td>&nbsp;</td>
+                </tr>
+
+                <tr>
+                    <td>HF Propagation</td>
+
+                    <td>&nbsp;</td>
+
+                    <td>&nbsp;</td>
+
+                    <td>&nbsp;</td>
+                </tr>
+
+                <tr>
+                    <td>HF Station Setup</td>
+
+                    <td>Dean Lang – WA9AZK</td>
+
+                    <td>&nbsp;</td>
+
+                    <td>&nbsp;</td>
+                </tr>
+
+                <tr>
+                    <td>Homebrew</td>
+
+                    <td>Chuck Johnson – WA7JOS
+                    <br />Ron Speirs – K7RLS</td>
+
+                    <td>&nbsp;</td>
+
+                    <td>&nbsp;</td>
+                </tr>
+
+                <tr>
+                    <td>IRLP</td>
+
+                    <td>&nbsp;</td>
+
+                    <td>&nbsp;</td>
+
+                    <td>&nbsp;</td>
+                </tr>
+
+                <tr>
+                    <td>License Upgrade and Renewals</td>
+
+                    <td>Gordon Smith – K7HFV</td>
+
+                    <td><a href="mailto:k7hfv@arrl.net">k7hfv@arrl.net</a></td>
+
+                    <td>&nbsp;</td>
+                </tr>
+
+                <tr>
+                    <td>Linux for Radio &amp; Bash Scripting</td>
+
+                    <td>Brent House – KD7ICT</td>
+
+                    <td>&nbsp;</td>
+
+                    <td>&nbsp;</td>
+                </tr>
+
+                <tr>
+                    <td>Mobile Installation</td>
+
+                    <td>Jerry Wellman – W7SAR</td>
+
+                    <td>&nbsp;</td>
+
+                    <td>&nbsp;</td>
+                </tr>
+
+                <tr>
+                    <td>Mountain Topping</td>
+
+                    <td>&nbsp;</td>
+
+                    <td>&nbsp;</td>
+
+                    <td>&nbsp;</td>
+                </tr>
+
+                <tr>
+                    <td>Operating Practices</td>
+
+                    <td>Dean Lang – WA9AZK</td>
+
+                    <td>&nbsp;</td>
+
+                    <td>&nbsp;</td>
+                </tr>
+
+                <tr>
+                    <td>Packet Radio</td>
+
+                    <td>Jerry Wellman – W7SAR</td>
+
+                    <td>&nbsp;</td>
+
+                    <td>&nbsp;</td>
+                </tr>
+
+                <tr>
+                    <td>QRP&nbsp;&nbsp;&nbsp; Andrew Madsen – AC7CF</td>
+
+                    <td>&nbsp;</td>
+
+                    <td>&nbsp;</td>
+
+                    <td>&nbsp;</td>
+                </tr>
+
+                <tr>
+                    <td>Repeater Construction &amp; Maintenance</td>
+
+                    <td>Clint Turner – KA7OEI
+                    <br />John Lloyd – K7JL</td>
+
+                    <td>&nbsp;</td>
+
+                    <td>&nbsp;</td>
+                </tr>
+
+                <tr>
+                    <td>RFI</td>
+
+                    <td>&nbsp;</td>
+
+                    <td>&nbsp;</td>
+
+                    <td>&nbsp;</td>
+                </tr>
+
+                <tr>
+                    <td>Satellite</td>
+
+                    <td>Ron Speirs – K7RLS</td>
+
+                    <td>&nbsp;</td>
+
+                    <td>&nbsp;</td>
+                </tr>
+
+                <tr>
+                    <td>Slow Scan TV</td>
+
+                    <td>John Hays – K7VE</td>
+
+                    <td>&nbsp;</td>
+
+                    <td>&nbsp;</td>
+                </tr>
+
+                <tr>
+                    <td>Software Defined Radio</td>
+
+                    <td>&nbsp;</td>
+
+                    <td>&nbsp;</td>
+
+                    <td>&nbsp;</td>
+                </tr>
+
+                <tr>
+                    <td>Special Events</td>
+
+                    <td>C Wesley Wilkinson, JR. – W7WES</td>
+
+                    <td>&nbsp;</td>
+
+                    <td>&nbsp;</td>
+                </tr>
+
+                <tr>
+                    <td>Spread Spectrum</td>
+
+                    <td>&nbsp;</td>
+
+                    <td>&nbsp;</td>
+
+                    <td>&nbsp;</td>
+                </tr>
+
+                <tr>
+                    <td>Traffic Handling</td>
+
+                    <td>&nbsp;</td>
+
+                    <td>&nbsp;</td>
+
+                    <td>&nbsp;</td>
+                </tr>
+
+                <tr>
+                    <td>VHF Contesting</td>
+
+                    <td>John Hays – K7VE</td>
+
+                    <td>&nbsp;</td>
+
+                    <td>&nbsp;</td>
+                </tr>
+
+                <tr>
+                    <td>VHF Digital</td>
+
+                    <td>John Hays – K7VE
+                    <br />Ron Speirs – K7RLS</td>
+
+                    <td>&nbsp;</td>
+
+                    <td>&nbsp;</td>
+                </tr>
+
+                <tr>
+                    <td>VHF Station Setup</td>
+
+                    <td>Ron Speirs – K7RLS</td>
+
+                    <td>&nbsp;</td>
+
+                    <td>&nbsp;</td>
+                </tr>
+            </table>
+        </div>
     )
 }
