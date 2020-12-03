07b6267735d021f06f4985cf290573958e6fd117
mr-cade <cadelpetersen@gmail.com>
2020-12-03 14:50:09 -0700

    book list added

--- client/src/components/Book-table/index.js
+++ client/src/components/Book-table/index.js
@@ -4,27 +4,40 @@ import "./style.css";
 export default function BookTable() {
     return (
         <div>
-            <h2 align="center">ARRL Books Available</h2>
-
-            <p>
-                The American Radio Relay League (ARRL) is the primary source of publications about
-                amateur radio in the United States. Some decades back there were radio stores that stocked
-                these books locally, but as they faded away UARC decided to become a dealer and make make them,
-                again, available locally.
-            </p>
-            <p>
-                Normally, these publications would be available at regular UARC meetings. However, while
-                the Coronavirus is restricting us to on-line meetings, here is another way to obtain them. To purchase any of these items, please contact Rick, our book manager, either
-                via e-mail (<a href="mailto:rickg916@gmail.com">rickg916@gmail.com)</a> or txt
-                (801-582-7783).  Phone calls from unknown numbers are generally not answered,
-                but voice mail will be checked. In your message please include the item names
-                and the preferred method of reply.
-            </p>
             <table border="1" align="center">
-            <tr><th>TITLE</th><th>ISBN</th><th>PUB<br/>Year</th><th>PRICE*</th></tr>
-            <tr><td>Amateur Radio Transceiver Performance Testing<br/></td><td>9781910190086&nbsp;</td><td>2014</td><td align="center">19.95</td></tr>
+                <tr><th>TITLE</th><th>ISBN</th><th>PUB<br />Year</th><th>PRICE*</th></tr>
+                <tr><td>Amateur Radio Transceiver Performance Testing<br /></td><td>9781910190086&nbsp;</td><td>2014</td><td align="center">19.95</td></tr>
+                <tr><td>An Introduction to Antenna Modeling</td><td>9781910193006&nbsp;</td><td>2014</td><td align="center">19.95</td></tr>
+                <tr><td>Arduino Sketches: tools & techniques for programming wizardry</td><td>9781118919606&nbsp;</td><td>2015</td><td align="center">35.00</td></tr>
+                <tr><td>ARRL Handbook 2020, 97th Ed., softcover</td><td>9781625951076&nbsp;</td><td>2019</td><td align="center">49.95</td></tr>
+                <tr><td>ARRL's Best of the Doctor is In, Vol. 1</td><td>9781625950741&nbsp;</td><td>2017</td><td align="center">19.95</td></tr>
+                <tr><td>ARRL's Hands-On Radio Experiments, Vol. 3</td><td>9781625950796&nbsp;</td><td>2018</td><td align="center">22.95</td></tr>
+                <tr><td>ARRL's Small Antennas for Small Spaces, 2nd Ed.</td><td>9781625950512&nbsp;</td><td>2019</td><td align="center">22.95</td></tr>
+                <tr><td>Basic Antennas: Understanding Practical Antennas and Designs</td><td>9780872599994&nbsp;</td><td>2009</td><td align="center">29.95</td></tr>
+                <tr><td>Batteries in a Portable World, 4th Ed.</td><td>9780968211847&nbsp;</td><td>2016</td><td align="center">29.95</td></tr>
+                <tr><td>Circuit Simulation and Analysis</td><td>9781625950055&nbsp;</td><td>2013</td><td align="center">27.95</td></tr>
+                <tr><td>Computers in Amateur Radio, 2nd Ed. w/CDROM</td><td>9781905086856&nbsp;</td><td>2013</td><td align="center">24.95</td></tr>
+                <tr><td>FCC Rules and Regulations for the Amateur Radio Service, 4th Ed.</td><td>9781625950765&nbsp;</td><td>2017</td><td align="center">7.95</td></tr>
+                <tr><td>Get on the Air with HF Digital, 2nd Ed.</td><td>9781625950833&nbsp;</td><td>2018</td><td align="center">19.95</td></tr>
+                <tr><td>Ham Radio for Arduino and PICAXE</td><td>9780872593244&nbsp;</td><td>2013</td><td align="center">29.95</td></tr>
+                <tr><td>High Speed Multimedia for Amateur Radio</td><td>9781625950529&nbsp;</td><td>2016</td><td align="center">27.95</td></tr>
+                <tr><td>Hints & Kinks for the Radio Amateur</td><td>9780872595200&nbsp;</td><td>2012</td><td align="center">19.95</td></tr>
+                <tr><td>International Antennas</td><td>9781910193358&nbsp;</td><td>2017</td><td align="center">19.95</td></tr>
+                <tr><td>License: General Class License Manual, 9th Ed., spiral-bound</td><td>9781625951069&nbsp;</td><td>2019</td><td align="center">32.95</td></tr>
+                <tr><td>License: Ham Radio License Manual, 4th Ed., Spiral-Bound</td><td>9781625950826&nbsp;</td><td>2018</td><td align="center">32.95</td></tr>
+                <tr><td>Magic Band Antennas for Ham Radio</td><td>9781625950987&nbsp;</td><td>2018</td><td align="center">19.95</td></tr>
+                <tr><td>Magnetic Loop Antennas</td><td>9788026099529&nbsp;</td><td>2019</td><td align="center">34.95</td></tr>
+                <tr><td>More Vertical Antenna Classics</td><td>9780872599796&nbsp;</td><td>2006</td><td align="center">17.95</td></tr>
+                <tr><td>Personal Emergency Communications</td><td>9780983505310&nbsp;</td><td>2012</td><td align="center">14.95</td></tr>
+                <tr><td>Propagation and Radio Science</td><td>9781625950277&nbsp;</td><td>2015</td><td align="center">27.95</td></tr>
+                <tr><td>Radio Science for the Radio Amateur</td><td>9780872593381&nbsp;</td><td>2013</td><td align="center">24.95</td></tr>
+                <tr><td>Receiving Antennas for the Radio Amateur</td><td>9781625950789&nbsp;</td><td>2018</td><td align="center">24.95</td></tr>
+                <tr><td>SDR Software Defined Radio, 2nd Ed.</td><td>9781910193464&nbsp;</td><td>2017</td><td align="center">24.95</td></tr>
+                <tr><td>The Care and Feeding of Transmission Lines</td><td>9780872594784&nbsp;</td><td>2012</td><td align="center">27.95</td></tr>
+                <tr><td>The Complete DX'er, 3rd Ed.</td><td>   0961757701</td><td>2003</td><td align="center">19.95</td></tr>
+                <tr><td>The Radio Today Guide to HF Data on FT8 & PSK</td><td>9781910193709&nbsp;</td><td>2019</td><td align="center">14.95</td></tr>
+                <tr><td>Your Guide to HF Fun</td><td>n/a</td><td>2001</td><td align="center">16.00</td></tr>
             </table>
-            <h5>make this dynamically populate from db of books using lodash and link to storefront option to buy -- currently static</h5>
         </div>
     )
 }
--- client/src/pages/books.js
+++ client/src/pages/books.js
@@ -2,8 +2,25 @@ import React from 'react'
 import BookTable from '../components/Book-table'
 
 export default function Books() {
-    return(
+    return (
         <div>
-            <BookTable/>
+            <h2 align="center">ARRL Books Available</h2>
+
+            <p>
+                The American Radio Relay League (ARRL) is the primary source of publications about
+                amateur radio in the United States. Some decades back there were radio stores that stocked
+                these books locally, but as they faded away UARC decided to become a dealer and make make them,
+                again, available locally.
+            </p>
+            <p>
+                Normally, these publications would be available at regular UARC meetings. However, while
+                the Coronavirus is restricting us to on-line meetings, here is another way to obtain them. To purchase any of these items, please contact Rick, our book manager, either
+                via e-mail (<a href="mailto:rickg916@gmail.com">rickg916@gmail.com)</a> or txt
+                (801-582-7783).  Phone calls from unknown numbers are generally not answered,
+                but voice mail will be checked. In your message please include the item names
+                and the preferred method of reply.
+            </p>
+            <BookTable />
         </div>
-        )}
+    )
+}
