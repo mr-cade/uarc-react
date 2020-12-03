c8d4bd7a54efd0e40bade1444549c4e43a011377
mr-cade <cadelpetersen@gmail.com>
2020-12-03 13:46:35 -0700

    boilerplate backend structure

---
+++ models/members.js
@@ -0,0 +1,21 @@
+module.exports = function (sequelize, DataTypes) {
+    var Member = sequelize.define("Member", {
+        fName: DataTypes.STRING,
+        lName: DataTypes.STRING,
+        callsign: DataTypes.STRING,
+        streetAddress: DataTypes.STRING,
+        city: DataTypes.STRING,
+        email: DataTypes.EMAIL,
+        expDate: DataTypes.DATE
+    });
+
+    Member.associate = function (models) {
+        // Associating Member with Posts
+        // When an Member is deleted, also delete any associated Posts
+        Member.hasMany(models.Post, {
+            onDelete: "cascade"
+        });
+    };
+
+    return Member;
+};
---
+++ schema.sql
@@ -0,0 +1,2 @@
+DROP DATABASE IF EXISTS members;
+CREATE DATABASE members;
