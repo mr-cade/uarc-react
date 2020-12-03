0a8fc6e67843eb9a290bf804b9b8e55db84c535d
mr-cade <cadelpetersen@gmail.com>
2020-12-03 13:49:49 -0700

    api routes

---
+++ routes/api-routes.js
@@ -0,0 +1,46 @@
+var db = require("../models");
+
+module.exports = function(app) {
+    app.get("/api/members", function(req, res) {
+      // Here we add an "include" property to our options in our findAll query
+      // We set the value to an array of the models we want to include in a left outer join
+      // In this case, just db.Post
+      db.Member.findAll({
+        include: [db.Post]
+      }).then(function(dbMember) {
+        res.json(dbMember);
+      });
+    });
+  
+    app.get("/api/members/:id", function(req, res) {
+      // Here we add an "include" property to our options in our findOne query
+      // We set the value to an array of the models we want to include in a left outer join
+      // In this case, just db.Post
+      db.Member.findOne({
+        where: {
+          id: req.params.id
+        },
+        include: [db.Post]
+      }).then(function(dbMember) {
+        res.json(dbMember);
+      });
+    });
+  
+    app.post("/api/members", function(req, res) {
+      db.Member.create(req.body).then(function(dbMember) {
+        res.json(dbMember);
+      });
+    });
+  
+    app.delete("/api/authors/:id", function(req, res) {
+      db.Member.destroy({
+        where: {
+          id: req.params.id
+        }
+      }).then(function(dbMember) {
+        res.json(dbMember);
+      });
+    });
+  
+  };
+  
