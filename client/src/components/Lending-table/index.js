6642442ce6f6ccdd35d68ada1e4cde64886c7eee
mr-cade <cadelpetersen@gmail.com>
2020-12-03 15:05:52 -0700

    lending table

--- client/src/components/Lending-table/index.js
+++ client/src/components/Lending-table/index.js
@@ -3,8 +3,38 @@ import "./style.css";
 
 export default function LendingTable() {
     return (
-     <div>
-         Lending Content
-     </div>   
+        <div>
+            <table>
+                <thead>
+                    <tr><th>Book Title</th><th>Pub.<br /> Year</th><th>ISBN</th><th>MSRP</th></tr>
+                </thead>
+                <tbody>
+                    <tr><td>200 Meters and Down: The Story of Amateur Radio</td><td>2018</td><td>9780872590014</td><td>15.95</td></tr>
+                    <tr><td>73: Vertical, Beam, and Triangle Antennas</td><td>1970</td><td>n/a</td><td>5.50</td></tr>
+                    <tr><td>A History of QST, Vol. 1: Amateur Radio Technology, 1915-2013</td><td>2013</td><td>9781625950031</td><td>34.95</td></tr>
+                    <tr><td>A History of QST, Vol. 2: Advertising, 2015-2013</td><td>2013</td><td>9781625950048</td><td>34.95</td></tr>
+                    <tr><td>ARRL Digital Technology for Emergency Communications Course (CD-ROM)&nbsp;</td><td>2008</td><td>9780872591240</td><td>49.95</td></tr>
+                    <tr><td>ARRL RFI Book: Practical Cures for Radio Frequency Interference, 3rd Ed.</td><td>2010</td><td>9780872590915</td><td>29.95</td></tr>
+                    <tr><td>ComTrain’s Tribute to Safety: Tower Climbing Safety and Rescue, 4th Ed.</td><td>1997</td><td>n/a</td><td>29.95</td></tr>
+                    <tr><td>Ham Radio for the New Ham: What to do the minute you get your … license</td><td>2019</td><td>97810914021719.99</td><td>9.99</td></tr>
+                    <tr><td>Introduction to Radio Frequency Design</td><td>1982</td><td>0872594920</td><td>39.95</td></tr>
+                    <tr><td>Microwave Know How for the Radio Amateur</td><td>2010</td><td>978190508656621.95&nbsp;</td>    <td>21.95</td></tr>
+                    <tr><td>Microwave Projects 2</td><td>2005</td><td>9781905086092</td><td>29.95</td></tr>
+                    <tr><td>Power Supply Handbook</td><td>2006</td><td>9781905086214</td><td>29.95</td></tr>
+                    <tr><td>PR-101: ARRL Public Relations (CD-ROM)</td><td>2009</td><td>n/a</td></tr>
+                    <tr><td>Practical Radio Frequency Test & Measurement: A Technician’s Handbook</td><td>2002</td><td>978075067161366.95</td><td>66.95</td></tr>
+                    <tr><td>The Amateur Radio Mobile Handbook </td><td>2001</td><td>9781905086719</td><td>29.95</td></tr>
+                    <tr><td>The ARRL Antenna Book, 15th Ed.</td><td>1988</td><td>0872592065</td><td>18.00</td></tr>
+                    <tr><td>The ARRL Antenna Book, 19th Ed.</td><td>2000</td><td>0872598047</td><td>30.00</td></tr>
+                    <tr><td>The ARRL Antenna Compendium, Vol. 2</td><td>1989</td><td>0872592545</td><td>14.00</td></tr>
+                    <tr><td>The ARRL Goes to Washington [CD-Rom (10-minute video)]</td><td>2005</td><td>n/a</td></tr>
+                    <tr><td>The ARRL Handbook for Radio Communications –– 2005, 82nd Ed.</td><td>2004</td><td>0872599280</td><td>39.95</td></tr>
+                    <tr><td>The ARRL Handbook for Radio Communications –– 2006, 83rd Ed.</td><td>2005</td><td>0872599485</td><td>39.95</td></tr>
+                    <tr><td>The ARRL Operating Manual for Radio Amateurs, 10th Ed.</td><td>2012</td><td>9780872595965</td><td>34.95</td></tr>
+                    <tr><td>The ARRL Operating Manual for Radio Amateurs, 8th Ed.</td><td>2006</td><td>087259913229</td><td>25.95</td></tr>
+                    <tr><td>Understanding Your Antenna Analyzer</td><td>2013</td><td>9780872592889</td><td>25.95</td></tr>
+                </tbody>
+            </table>
+        </div>
     )
 }
