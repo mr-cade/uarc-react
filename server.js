d361663ad40a7b20357cb6a69e25e456c1d3648c
mr-cade <cadelpetersen@gmail.com>
2021-03-26 17:18:24 -0600

    prod build used

--- server.js
+++ server.js
@@ -42,7 +42,7 @@ app.use(favicon(__dirname + '/build/favicon.ico'));
 app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, 'build')));
 app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
 // if (process.env.NODE_ENV === "production") {
